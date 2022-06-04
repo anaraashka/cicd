67dc4ae13dfb817fb6594e86237e9006a4faafd1
anaraashka <anaraashka@yahoo.com>
2022-06-04 15:38:24 -0500

    Subscription added to SNS, target is my email

--- monitoring.tf
+++ monitoring.tf
@@ -1,3 +1,27 @@
 resource "aws_sns_topic" "EC2_STATE" {
   name = "EC2_STATE_CHANGE"
 }
+
+resource "aws_sns_topic_subscription" "ec2_sns_target" {
+  topic_arn = aws_sns_topic.EC2_STATE.arn
+  protocol  = "email"
+  endpoint  = "anaraashka@yahoo.com"
+}
+
+resource "aws_sns_topic_policy" "default" {
+  arn    = aws_sns_topic.EC2_STATE.arn
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["SNS:Publish"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+    resources = [aws_sns_topic.EC2_STATE.arn]
+  }
+}
